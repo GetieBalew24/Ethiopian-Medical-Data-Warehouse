b4d31a773fd046ca1f84bc8670b2a430f932b219
GetieBalew24 <getiebalew20@gmail.com>
2025-01-31 01:06:28 +0300

    standadardizing formats

--- scripts/data_cleaning.py
+++ scripts/data_cleaning.py
@@ -25,9 +25,16 @@ class DataCleaner:
         self.df = self.df.drop_duplicates(subset='message_id', keep='first')
         after_removal = len(self.df)
         logging.info(f"Removed {before_removal - after_removal} duplicate rows.")
+        
     def handle_missing_values(self):
         """Handles missing values by filling or dropping them."""
         before_removal = len(self.df)
         self.df = self.df.dropna(subset=['message_id', 'text', 'sender', 'channel', 'date'])
         after_removal = len(self.df)
         logging.info(f"Removed {before_removal - after_removal} rows due to missing values.")
+
+    def standazrdize_formats(self):
+        """Standardizes the formats, for example, converting date to standard format."""
+        self.df['date'] = pd.to_datetime(self.df['date'], errors='coerce')  # Ensures date is in datetime format
+        self.df['text'] = self.df['text'].str.strip()  # Removes leading/trailing spaces
+        logging.info("Standardized date formats and cleaned text fields.")
